331ba5027a995b9b5e4235492557aeff419aff76
Vegicode <vadimegiazarov@mail.ru>
2025-02-13 10:53:00 +0300

    Поменял интерфейс на SwiftUI

---
+++ TravelSchedule/TravelSchedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  TravelSchedule
+//
+//  Created by Mac on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup{
+            ContentView()
+        }
+    }
+}
